ea7518d0000edd2ebfe900f48f037a5be794aeb1
basel19-meet <basel19@meet.mit.edu>
2017-08-07 10:26:04 +0300

    Stopping here

--- dominica.py
+++ dominica.py
@@ -2,18 +2,45 @@ import random
 import turtle
 
 food = turtle.clone()
- 
+turtle.penup()
 size_x = 800
 size_y = 700
 turtle.setup(size_x,size_y)
 square_size = 20
-food_list = ['a','b','c','d','e']
-for n in range(22):
-    random_index = random.randint(0, len(food_list) - 1)
-    print(food_list[random_index])
-    
+food_list = ['square','triangle','circle']
+food_clones = []
+for n in range(len(food_list)):
+##    turtle.register_shape(food_list[n])
+    obj = turtle.clone()
+    obj.hideturtle()
+    food_clones.append(obj)
+    obj.shape(food_list[n])
 
-def make_food():
+def set_up(food_clones):
+    """
+    For every clone in the list,
+    it will position it at random
+    positions along a horizontal line
+    at y = 200
+    """
+    taken_x = []
+    for clone in food_clones:
+        min_x=int(size_x/2/square_size)+1
+        max_x=int(size_x/2/square_size)-1
+        print(min_x, max_x)
+        food_x=random.randint(min_x,max_x)*square_size
+        while food_x in taken_x:
+            min_x=int(size_x/2/square_size)+1
+            max_x=int(size_x/2/square_size)-1
+            food_x=random.randint(min_x,max_x)*square_size
+        clone.goto(food_x, 200)
+        clone.showturtle()
+
+
+set_up(food_clones)
+
+
+def make_food(y):
     min_x=int(size_x/2/square_size)+1
     max_x=int(size_x/2/square_size)-1
     food_x=random.randint(min_x,max_x)*square_size
