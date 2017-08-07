f0bcfa837fd3afc4c92e0e39d15eb7aa99870c94
basel19-meet <basel19@meet.mit.edu>
2017-08-07 13:53:13 +0300

    Stopping here

--- Asaf.py
+++ Asaf.py
@@ -7,15 +7,15 @@ turtle.setup(x_size,y_size)
 SQUARE_SIZE=20
 
 farmer_pos_list=[]
-farmer_stamp_list=[]
 
-turtle.hideturtle
+turtle.hideturtle()
 turtle.register_shape("farmer.gif")
 farmer=turtle.clone()
 farmer.shape("farmer.gif")
 
 farmer.penup()
-farmer.goto(0,-350)
+farmer.goto(0,-320)
+farmer.showturtle()
 
 LEFT_ARROW="Left"
 RIGHT_ARROW="Right"
@@ -23,35 +23,40 @@ LEFT=0
 RIGHT=1
 direction=LEFT
 
-UP_EDGE = 350
-DOWN_EDGE = -350
-RIGHT_EDGE = 400
-LEFT_EDGE = -400
+RIGHT_EDGE = 380
+LEFT_EDGE = -380
 
 def left():
+    
     global direction
     direction=LEFT
-    x=farmer.pos()[0]
-    y=farmer.pos()[1]
-    farmer.goto(x-SQUARE_SIZE,y)
+    move_farmer()
     print('you moved the farmer laft')
     print(farmer.pos())
-##    farmer_stamp_list.append(farmer.stamp())
-##    farmer_stamp_list.pop(0)
-##    print(farmer_stamp_list)
     
 def right():
+    
     global direction
     direction=RIGHT
-    x=farmer.pos()[0]
-    y=farmer.pos()[1]
-    farmer.goto(x+SQUARE_SIZE,y)
+    move_farmer()
     print('you moved the farmer right')
     print(farmer.pos())
-##    farmer_stamp_list.append(farmer.stamp())
-##    farmer_stamp_list.pop(0)
-##    print(farmer_stamp_list)
+
+
+
+def move_farmer():
+    x=farmer.pos()[0]
+    y=farmer.pos()[1]
     
+    if direction == LEFT:
+        farmer.goto(x-SQUARE_SIZE,y)
+    elif direction == RIGHT:
+        farmer.goto(x+SQUARE_SIZE,y)
+    if x>RIGHT_EDGE:
+        quit()
+    elif x<LEFT_EDGE:
+        quit()
+
 turtle.onkeypress(left,LEFT_ARROW)
 turtle.onkeypress(right,RIGHT_ARROW)
 turtle.listen()
