71cfdc6349f536e2a70ae072f175bd2b244cc1c8
basel19-meet <basel19@meet.mit.edu>
2017-08-09 13:18:18 +0300

    Stopping here

--- shelly.py
+++ shelly.py
@@ -22,10 +22,10 @@ def farmer():
     RIGHT=1
     direction=LEFT
 
-    RIGHT_EDGE = 200
-    LEFT_EDGE = -200
-    RIGHT_EDGE = 380
-    LEFT_EDGE = -380
+    RIGHT_EDGE = 400
+    LEFT_EDGE = -400
+    RIGHT_EDGE = 350
+    LEFT_EDGE = -350
 
     def left():
         
@@ -61,59 +61,60 @@ def farmer():
     turtle.listen()
 
 
-#changing shape
-#a = the names of all the items
-happy = turtle.clone()
-sad = turtle.clone()
-happy.register_shape("happy_face.gif")
-sad.register_shape("sad_face.gif")
-
-a = ["apple.gif", "banana.gif", "strawberry.gif", "orange.gif", "grape.gif"]
-
-a.sort()
-
-clones_list = []
-for n in range(len(a)):
-    obj = turtle.clone()
-    clones_list.append(obj)
-    turtle.register_shape(a[n])
-    obj.shape(a[n])
-
-
-
-##good_food_turtles
-##good_food_names
+###changing shape
+###a = the names of all the items
+##happy = turtle.clone()
+##sad = turtle.clone()
+##happy.register_shape("happy_face.gif")
+##sad.register_shape("sad_face.gif")
 ##
-##bad_food_turtles
-##bad_food_names
+##a = ["apple.gif", "banana.gif", "strawberry.gif", "orange.gif", "grape.gif"]
 ##
-##other_stuff_turtles
-##other_stuff_names
-
-#good food
-for good_food in clones_list:
-    if good_food.pos() == farmer.pos():
-        score = score + 1
-        happy.stamp()
-        print("Yay!")
-
-#bad food
-for bad_food in clones_list:
-    if bad_food.pos() == farmer.pos():
-        score = score - 1
-        sad.stamp()
-        print("Oh no!")
-
-#hit the edge
-for good_food in clones_list:
-    if good_food.pos()>=RIGHT_EDGE:
-        score = score - 1
-        sad.stamp()
-        print("Oh no!")
-
-#other stuff
-for other_stuff in clones_list:
-    if other_stuff.pos() == farmer.pos():
-        print("Game over!")
-        quit()
-
+##a.sort()
+##
+##clones_list = []
+##for n in range(len(a)):
+##    obj = turtle.clone()
+##    clones_list.append(obj)
+##    turtle.register_shape(a[n])
+##    obj.shape(a[n])
+##
+##
+##
+####good_food_turtles
+####good_food_names
+####
+####bad_food_turtles
+####bad_food_names
+####
+####other_stuff_turtles
+####other_stuff_names
+##
+###good food
+##for good_food in clones_list:
+##    if good_food.pos() == farmer.pos():
+##        score = score + 1
+##        happy.stamp()
+##        print("Yay!")
+##
+###bad food
+##for bad_food in clones_list:
+##    if bad_food.pos() == farmer.pos():
+##        score = score - 1
+##        sad.stamp()
+##        print("Oh no!")
+##
+###hit the edge
+##for good_food in clones_list:
+##    if good_food.pos()>=RIGHT_EDGE:
+##        score = score - 1
+##        sad.stamp()
+##        print("Oh no!")
+##
+###other stuff
+##for other_stuff in clones_list:
+##    if other_stuff.pos() == farmer.pos():
+##        print("Game over!")
+##        quit()
+##
+##
